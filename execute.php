8fd02e636b0673a0a1996a659cc4b4a43f91c0a5
Matteo Pipitone <matteopipitone93@gmail.com>
2016-11-07 11:57:58 +0000

    Pushed from Dropbox

--- execute.php
+++ execute.php
@@ -8,53 +8,16 @@ if(!$update)
 {
   exit;
 }
-// assegno alle seguenti variabili il contenuto ricevuto da Telegram
 $message = isset($update['message']) ? $update['message'] : "";
-$messageId = isset($message['message_id']) ? $message['message_id'] : "";
 $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
-$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
-$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
-$username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
-$date = isset($message['date']) ? $message['date'] : "";
 $text = isset($message['text']) ? $message['text'] : "";
 $botUrl = "https://api.telegram.org/bot" . BOT_TOKEN . "/sendPhoto";
-// pulisco il messaggio ricevuto togliendo eventuali spazi prima e dopo il testo
-$text = trim($text);
-// converto tutti i caratteri alfanumerici del messaggio in minuscolo
-$text = strtolower($text);
-// mi preparo a restitutire al chiamante la mia risposta che è un oggetto JSON
-// imposto l'header della risposta
-header("Content-Type: application/json");
-$response = '';
-if(strpos($text, "/start") === 0 || $text=="buongiorno")
-{
-	$response = "Buongiornissimo $firstname, kaffeee!!11!!";
-}
-elseif($text=="politica")
-{
-	$response = "E renzi ke faaa????";
-}
-elseif($text=="salvini")
-{
-	$response = "RUSPA!!";
-}else if($text=="immagine"){
-	$postFields = array('chat_id' => $chatId, 'photo' => new CURLFile(realpath("image1.jpg")), 'caption' => $text);
-	$ch = curl_init(); 
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type:multipart/form-data"));
-	curl_setopt($ch, CURLOPT_URL, $botUrl); 
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
-	$output = curl_exec($ch)
-}else
-{
-	//$response = "Comando non valido!";
-}
-
-// la mia risposta è un array JSON composto da chat_id, text, method
-// chat_id mi consente di rispondere allo specifico utente che ha scritto al bot
-// text è il testo della risposta
-$parameters = array('chat_id' => $chatId, "text" => $response);
-// method è il metodo per l'invio di un messaggio (cfr. API di Telegram)
-$parameters["method"] = "sendMessage";
-// converto e stampo l'array JSON sulla response
-echo json_encode($parameters);
+// change image name and path
+$postFields = array('chat_id' => $chatId, 'photo' => new CURLFile(realpath("image1.png")), 'caption' => $text);
+$ch = curl_init(); 
+curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type:multipart/form-data"));
+curl_setopt($ch, CURLOPT_URL, $botUrl); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
+// read curl response
+$output = curl_exec($ch);
