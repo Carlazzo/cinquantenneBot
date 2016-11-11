b43dcb121e7b45a195f48d7b1594741802ee08fb
Matteo Pipitone <matteo.pipitone@elco.local>
2016-11-11 16:01:56 +0100

    Modificata buona parte del progetto

--- execute.php
+++ execute.php
@@ -3,15 +3,6 @@
 define('BOT_TOKEN', '287099689:AAHEUsdjtrD2VN1vgWfJnXcq5TfSTrLLbUE');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
-$message = isset($update['message']) ? $update['message'] : "";
-$messageId = isset($message['message_id']) ? $message['message_id'] : "";
-$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
-$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
-$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
-$username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
-$date = isset($message['date']) ? $message['date'] : "";
-$text = isset($message['text']) ? $message['text'] : "";
-$botUrl = "https://api.telegram.org/bot" . BOT_TOKEN . "/sendPhoto";
 
 /***
  * @param $path
@@ -31,9 +22,20 @@ function sendPhotos($path, $chatId, $botUrl ){
     // read curl response
     return curl_exec($ch);
 }*/
+$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
+
 try {
 
-	// recupero il contenuto inviato da Telegram
+    $message = isset($update['message']) ? $update['message'] : "";
+    $messageId = isset($message['message_id']) ? $message['message_id'] : "";
+    $firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
+    $lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
+    $username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
+    $date = isset($message['date']) ? $message['date'] : "";
+    $text = isset($message['text']) ? $message['text'] : "";
+    $botUrl = "https://api.telegram.org/bot" . BOT_TOKEN . "/sendPhoto";
+
+    // recupero il contenuto inviato da Telegram
 	$content = file_get_contents("php://input");
 	// converto il contenuto da JSON ad array PHP
 	$update = json_decode($content, true);
